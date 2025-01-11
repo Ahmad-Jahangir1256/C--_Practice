43273b0b719c59ba3b0cad7070989cde8501b226
Ahmad Jahangir <ahmad.jahangir.1256@gmail.com>
2025-01-11 16:54:05 +0500

    update

---
+++ Functions/1st-function.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+// Function to add two numbers
+int add(int a, int b)
+{
+    return a + b;
+}
+
+// Function to subtract two numbers
+int sub(int a, int b)
+{
+    return a - b;
+}
+
+// Function to multiply two numbers
+int mul(int a, int b)
+{
+    return a * b;
+}
+
+// Function to divide two numbers
+int divide(int a, int b)
+{
+    if (b == 0)
+    {
+        cout << "Error: Division by zero" << endl;
+        return 0;
+    }
+    return a / b;
+}
+
+// Main function
+int main()
+{
+    // Declare variables
+    int a, b;
+
+    // Prompt user to enter the first number
+    cout << "Enter the first number: ";
+    cin >> a;
+
+    // Prompt user to enter the second number
+    cout << "Enter the second number: ";
+    cin >> b;
+
+    // Display the results of the arithmetic operations
+    cout << a << " + " << b << " = " << add(a, b) << endl;
+    cout << a << " - " << b << " = " << sub(a, b) << endl;
+    cout << a << " * " << b << " = " << mul(a, b) << endl;
+    cout << a << " / " << b << " = " << divide(a, b) << endl;
+
+    // Return 0 to indicate successful execution
+    return 0;
+}
